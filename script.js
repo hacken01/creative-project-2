c764c32855746c003ceffeb8e58f51a6ed6a4318
Gabriel Arancibia <gabriel@Arancibia.com>
2021-02-28 06:23:45 +0000

    lab working

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.getElementById("pokemonSubmit").addEventListener("click", function (eve
     if (pokeName === "")
         return;
     console.log(pokeName);
+    const name = 'flavio'
+
     const url = " https://pokeapi.co/api/v2/pokemon/" + pokeName;
     fetch(url)
         .then(function (response) {
@@ -11,12 +13,18 @@ document.getElementById("pokemonSubmit").addEventListener("click", function (eve
         }).then(function (json) {
             console.log(json);
             let results = "";
-            results += '<h2>' + json.name + "</h2>";
-            results += "<p> <h2>";
-
-            results += "base experience:  " + json.base_experience;
-
-            results += "<h2/></p>";
+            const nameCapitalized = json.name.charAt(0).toUpperCase() + json.name.slice(1)
+            results += '<h2>' + nameCapitalized + "</h2>";
+            results += "<p> <h4>";
+            results += "Base Experience: " + json.base_experience;
+            results += "<br/>Height: " + json.height;
+            results += "<br/>Weight: " + json.weight;
+            results += "<br/>Abilities: ";
+            for (let i = 0; i < json.abilities.length; i++) {
+                results += json.abilities[i].ability.name + ", ";
+            }
+            results = results.slice(0, -1);// remove the last comma
+            results += "<h4/></p>";
             results += '<img src="' + json.sprites.front_default + '"/>';
             document.getElementById("pokemonResults").innerHTML = results;
         });
