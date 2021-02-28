32015a64d2f0ce18bca9305d4387b5af9a45ec81
Gabriel Arancibia <gabriel@Arancibia.com>
2021-02-28 07:30:09 +0000

    lab done

--- script.js
+++ script.js
@@ -3,22 +3,25 @@ document.getElementById("pokemonSubmit").addEventListener("click", function (eve
     const pokeName = document.getElementById("pokemonInput").value;
     if (pokeName === "")
         return;
-    console.log(pokeName);
     const name = 'flavio'
-
+    let status = "";
     const url = " https://pokeapi.co/api/v2/pokemon/" + pokeName;
     fetch(url)
         .then(function (response) {
+            if(response.status == 404){
+                document.getElementById("pokemonResults").innerHTML = "No pokemon Found.";
+                document.getElementById("pokemonMoves").innerHTML = "Try Another Name";
+            }
             return response.json();
         }).then(function (json) {
             console.log(json);
             let results = "";
             let moves = "";
             const nameCapitalized = json.name.charAt(0).toUpperCase() + json.name.slice(1)
-            results += '<h2>' + nameCapitalized + "</h2>";
+            results += '<h1>' + nameCapitalized + "</h1>";
             results += '<img src="' + json.sprites.front_default + '"/>';
 
-            results += "<p> <h4>";
+            results += "<p> <h3>";
             results += "Base Experience: " + json.base_experience;
             results += "<br/>Height: " + json.height;
             results += "<br/>Weight: " + json.weight;
@@ -26,18 +29,16 @@ document.getElementById("pokemonSubmit").addEventListener("click", function (eve
             for (let i = 0; i < json.abilities.length; i++) {
                 results += json.abilities[i].ability.name + ", ";
             }
-            results = results.slice(0, -1);// remove the last comma
-            results += "<h4/></p>";
+            results = results.slice(0, -2);// remove the last comma
+            results += "<h3/></p>";
 
             moves += "<h2>List of moves</h2>";
-            moves += "<p> <h4>";
+            moves += "<p> <h3>";
             for (let i = 0; i < json.moves.length; i++) {
                 moves += i + 1 + " - " + json.moves[i].move.name + "<br/> ";
             }
-            moves += "<h4/></p>";
+            moves += "<h3/></p>";
             document.getElementById("pokemonResults").innerHTML = results;
             document.getElementById("pokemonMoves").innerHTML = moves;
         });
-
-
 });
